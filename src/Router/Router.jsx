c4a8571ea7a7d51e50dfadf8bf0578fe75c57bbd
Saara Khan <khansarah0716@gmail.com>
2025-08-15 17:44:18 +0530

    fix /login /signup after deployemnt

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,7 +2,6 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Login from "../components/Auth/Login";
 import Signup from "../components/Auth/Signup";
 import Home from "../components/Home";
-import PrivateRoute from "../components/PrivateRoute ";
 const Router = () => {
   return (
     <Routes>
